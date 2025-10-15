cf94f9da8747e5283658eca7a94a0efe01c46ddc
toddynnn <86982643+ToddyTheNoobDud@users.noreply.github.com>
2025-10-15 09:15:42 -0300

    rename filters

--- Salad/__init__.py
+++ Salad/__init__.py
@@ -1,8 +1,8 @@
 __title__ = 'Salad'
 __author__ = 'ToddyTheNoobDud'
 __contibutors__ = 'southctrl'
-__version__ = '1.1.0'
-__all__ = ['Rest', 'Node', 'Salad', 'Player', 'Queue', 'Track', 'EventEmitter', 'Filters', 'PlayerStateManager', 'SaladVoiceClient', 'Lyrics']
+__version__ = '1.2.0'
+__all__ = ['Rest', 'Node', 'Salad', 'Player', 'Queue', 'Track', 'EventEmitter', 'AudioFilters', 'PlayerStateManager', 'SaladVoiceClient', 'Lyrics']
 
 from .Rest import Rest
 from .Node import Node
@@ -11,7 +11,7 @@ from .Player import Player
 from .Queue import Queue
 from .Track import Track
 from .EventEmitter import EventEmitter
-from .Filters import Filters
 from .PlayerStateManager import PlayerStateManager
 from .voiceclient import SaladVoiceClient
 from .Lyrics import Lyrics
+from .Filters import AudioFilters
